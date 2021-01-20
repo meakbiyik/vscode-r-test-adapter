ebaf7147846b4b12441aeaba8dcb92a041bd713e
M. Eren Akbiyik <erenakbiyik@gmail.com>
2021-01-20 13:32:40 +0100

    Enhance Rscript path resolution

--- src/testthat/runner.ts
+++ src/testthat/runner.ts
@@ -1,6 +1,8 @@
 import { Uri, workspace } from "vscode";
 import * as util from "util";
 import * as path from "path";
+import * as winreg from "winreg";
+import * as fs from "fs";
 import * as tmp from "tmp-promise";
 import { exec } from "child_process";
 import * as vscode from "vscode";
@@ -9,12 +11,14 @@ import { TestInfo } from "vscode-test-adapter-api";
 import { parseTestsFromFile } from "./parser";
 import { appendFile as _appendFile } from "fs";
 import { TestthatAdapter } from "./adapter";
+import { lookpath } from "lookpath";
 
 const appendFile = util.promisify(_appendFile);
+let RscriptPath: string | undefined;
 
 export async function runAllTests(adapter: TestthatAdapter): Promise<string> {
     let devtoolsCall = `options("testthat.use_colours"=F);devtools::test('.')`;
-    let RscriptCommand = getRscriptCommand()
+    let RscriptCommand = await getRscriptCommand(adapter)
     let command = `${RscriptCommand} -e "${devtoolsCall}"`;
     let cwd = vscode.workspace.workspaceFolders![0].uri.fsPath;
     return new Promise(async (resolve, reject) => {
@@ -32,7 +36,7 @@ export async function runSingleTestFile(
     filePath: string
 ): Promise<string> {
     let devtoolsCall = `options("testthat.use_colours"=F);devtools::test_file('${filePath.replace(/\\/g, "/")}')`;
-    let RscriptCommand = getRscriptCommand()
+    let RscriptCommand = await getRscriptCommand(adapter)
     let command = `${RscriptCommand} -e "${devtoolsCall}"`;
     let cwd = vscode.workspace.workspaceFolders![0].uri.fsPath;
     return new Promise(async (resolve, reject) => {
@@ -82,14 +86,49 @@ export async function runTest(adapter: TestthatAdapter, test: TestInfo) {
         });
 }
 
-function getRscriptCommand() {
+async function getRscriptCommand(adapter: TestthatAdapter) {
     let config = vscode.workspace.getConfiguration("RTestAdapter")
-    let path = config.get("RscriptPath")
-    if(path == undefined || (<string>path).length == 0){
-        return "RScript"
-    } else {
-        return `"${path}"`
+    let configPath: string | undefined = config.get("RscriptPath")
+    if (configPath != undefined){
+        if ((<string>configPath).length > 0 && fs.existsSync(configPath)) return Promise.resolve(`"${configPath}"`)
+        else {
+            adapter.log.warn(`Rscript path given in the configuration ${configPath} is invalid. Falling back to defaults.`)
+        }
     }
+    if (RscriptPath != undefined) return Promise.resolve(`"${RscriptPath}"`);
+    RscriptPath = await lookpath('Rscript');
+    if (RscriptPath != undefined) return Promise.resolve(`"${RscriptPath}"`);
+    if (process.platform != "win32"){
+        let candidates = ["/usr/bin", "/usr/local/bin"]
+        for (const candidate of candidates) {
+            let possibleRscriptPath = path.join(candidate, "Rscript")
+            if (fs.existsSync(possibleRscriptPath)){
+                adapter.log.info(`found Rscript among candidate paths: ${possibleRscriptPath}`)
+                RscriptPath = possibleRscriptPath;
+                return Promise.resolve(`"${RscriptPath}"`);
+            }
+        }
+    } else {
+        try {
+            const key = new winreg({
+                hive: winreg.HKLM,
+                key: '\\Software\\R-Core\\R'
+            });
+            const item: winreg.RegistryItem = await new Promise((resolve, reject) =>
+                key.get('InstallPath', (err, result) => err ? reject(err) : resolve(result)));
+        
+            const rhome = item.value;
+        
+            let possibleRscriptPath = rhome + "\\bin\\Rscript.exe";
+            if (fs.existsSync(possibleRscriptPath)){
+                adapter.log.info(`found Rscript in registry: ${possibleRscriptPath}`)
+                console.log("found Rscript in registry:", possibleRscriptPath)
+                RscriptPath = possibleRscriptPath;
+                return Promise.resolve(`"${RscriptPath}"`);
+            }
+        } catch (e) {}
+    }
+    throw Error("Rscript could not be found in PATH, cannot run the tests.")
 }
 
 function getRangeOfTest(label: string, source: string) {
