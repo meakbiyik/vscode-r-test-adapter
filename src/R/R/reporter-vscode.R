b0d3064c7de4cc869545b23a255fb14a4bdaff2e
Kirill Müller <krlmlr@mailbox.org>
2021-03-23 06:22:04 +0100

    File output, remove TAP

--- src/R/R/reporter-vscode.R
+++ src/R/R/reporter-vscode.R
@@ -8,25 +8,27 @@
 #' @family reporters
 VsCodeReporter <- R6::R6Class("VsCodeReporter",
   inherit = Reporter,
+  private = list(
+    filename = NULL
+  ),
   public = list(
     suite_name = NULL,
-    results = list(),
-    n = 0L,
-    has_tests = FALSE,
-    contexts = NA_character_,
 
     initialize = function(suite_name, ...) {
       super$initialize(...)
       self$suite_name <- suite_name
+      private$filename <- NULL
       self$capabilities$parallel_support <- TRUE
+      # FIXME: self$capabilities$parallel_updates <- TRUE
     },
 
     start_reporter = function() {
       self$cat_json(list(type = "start_reporter", tests = list(self$suite_name)))
     },
 
-    start_context = function(context) {
-      self$contexts[self$n + 1] <- context
+    start_file = function(filename) {
+      self$cat_json(list(type = "start_file", filename = filename))
+      private$filename <- filename
     },
 
     start_test = function(context, test) {
@@ -42,40 +44,19 @@ VsCodeReporter <- R6::R6Class("VsCodeReporter",
         message = exp_message(result),
         location = expectation_location(result)
       ))
-      self$has_tests <- TRUE
-      self$n <- self$n + 1L
-      self$results[[self$n]] <- result
     },
 
     end_test = function(context, test) {
       self$cat_json(list(type = "end_test", context = context, test = test))
     },
 
+    end_file = function() {
+      self$cat_json(list(type = "end_file", filename = private$filename))
+      private$filename <- NULL
+    },
+
     end_reporter = function() {
       self$cat_json(list(type = "end_reporter", tests = list(self$suite_name)))
-      if (!self$has_tests) {
-        return()
-      }
-
-      self$cat_line("1..", self$n)
-      for (i in 1:self$n) {
-        if (!is.na(self$contexts[i])) {
-          self$cat_line("# Context ", self$contexts[i])
-        }
-        result <- self$results[[i]]
-        if (expectation_success(result)) {
-          self$cat_line("ok ", i, " ", result$test)
-        } else if (expectation_broken(result)) {
-          self$cat_line("not ok ", i, " ", result$test)
-          msg <- gsub("(^|\n)", "\\1  ", format(result))
-          self$cat_line(msg)
-        } else {
-          self$cat_line(
-            "ok ", i, " # ", toupper(expectation_type(result)), " ",
-            format(result)
-          )
-        }
-      }
     },
 
     cat_json = function(x) {
--- src/R/tests/testthat/_snaps/reporter-vscode.md
+++ src/R/tests/testthat/_snaps/reporter-vscode.md
@@ -1,6 +1,7 @@
 # reporter works
 
     {"type":"start_reporter","tests":["suite_name"]}
+    {"type":"start_file","filename":"reporters/tests.R"}
     {"type":"start_test","context":{},"test":"Success"}
     {"type":"add_result","context":{},"test":"Success","result":"success","message":"Success has been forced","location":"tests.R:2:3"}
     {"type":"end_test","context":{},"test":"Success"}
@@ -25,33 +26,6 @@
     {"type":"start_test","context":{},"test":"warnings get backtraces"}
     {"type":"add_result","context":{},"test":"warnings get backtraces","result":"warning","message":"def","location":"tests.R:37:3"}
     {"type":"end_test","context":{},"test":"warnings get backtraces"}
+    {"type":"end_file","filename":"reporters/tests.R"}
     {"type":"end_reporter","tests":["suite_name"]}
-    1..8
-    ok 1 Success
-    not ok 2 Failure:1
-      FALSE is not TRUE
-      
-      `actual`:   FALSE
-      `expected`: TRUE 
-    not ok 3 Failure:2a
-      FALSE is not TRUE
-      
-      `actual`:   FALSE
-      `expected`: TRUE 
-      Backtrace:
-       1. f() reporters/tests.R:11:2
-       2. testthat::expect_true(FALSE) reporters/tests.R:10:7
-    not ok 4 Error:1
-      Error: stop
-    not ok 5 errors get tracebacks
-      Error: !
-      Backtrace:
-       1. f() reporters/tests.R:23:2
-       2. g() reporters/tests.R:19:7
-       3. h() reporters/tests.R:20:7
-    ok 6 # SKIP Reason: skip
-    ok 7 # SKIP Reason: empty test
-    ok 8 # WARNING def
-    Backtrace:
-     1. f() reporters/tests.R:37:2
 
