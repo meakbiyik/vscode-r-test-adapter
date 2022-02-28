3bd1ebb418eff275a5d74ce9355bbbb32176f325
M. Eren Akbiyik <erenakbiyik@gmail.com>
2022-03-01 00:00:04 +0100

    Fomar type file

--- src/testthat/reporter.d.ts
+++ src/testthat/reporter.d.ts
@@ -2,19 +2,26 @@
  * Information about a test state as outputted by R reporter, in JSON format.
  */
 export interface TestResult {
-    type: 'start_reporter' | "start_file" | "start_test" | "add_result" | "end_test" | "end_file" | "end_reporter";
+    type:
+        | "start_reporter"
+        | "start_file"
+        | "start_test"
+        | "add_result"
+        | "end_test"
+        | "end_file"
+        | "end_reporter";
     /**
-     * Test filename if available
+     * Relative path of the test file if available
      */
     filename?: string;
     /**
      * Test label if available
      */
-    test?: string
+    test?: string;
     /**
      * Test result if available
      */
-    result?: 'success' | 'failure' | 'error' | 'skip' | 'warning';
+    result?: "success" | "failure" | "error" | "skip" | "warning";
     /**
      * This message will be displayed by the Test Explorer when the user selects the test.
      * It is usually used for information about why a test has failed.
