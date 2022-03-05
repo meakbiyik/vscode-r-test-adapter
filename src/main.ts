89690342bb8983c3959f7a6075f021e07cf2c83a
M. Eren Akbiyik <erenakbiyik@gmail.com>
2022-03-05 21:02:29 +0100

    Migrate from Test Explorer UI - Step 1
    Following https://github.com/connor4312/test-controller-migration-example/commits/master
    
    Remove the hooks to the test explorer extension.

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
 import * as vscode from "vscode";
-import { TestHub, testExplorerExtensionId } from "vscode-test-adapter-api";
-import { Log, TestAdapterRegistrar } from "vscode-test-adapter-util";
-import { TestthatAdapter } from "./testthat/adapter";
 
 export async function activate(context: vscode.ExtensionContext) {
-    const workspaceFolder = (vscode.workspace.workspaceFolders || [])[0];
-
-    // create a simple logger that can be configured with the configuration variables
-    // `RTestAdapter.logpanel` and `RTestAdapter.logfile`
-    const log = new Log("RTestAdapter", workspaceFolder, "R Test Adapter Log");
-    context.subscriptions.push(log);
-
-    // get the Test Explorer extension
-    const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
-    if (log.enabled) log.info(`Test Explorer ${testExplorerExtension ? "" : "not "}found`);
-
-    if (testExplorerExtension) {
-        const testHub = testExplorerExtension.exports;
-
-        // this will register an RTestAdapter for each WorkspaceFolder
-        context.subscriptions.push(
-            new TestAdapterRegistrar(
-                testHub,
-                (workspaceFolder) => new TestthatAdapter(workspaceFolder, log),
-                log
-            )
-        );
-    }
+    const controller = vscode.tests.createTestController("r-test-adapter", "R Test Adapter");
+    context.subscriptions.push(controller);
 }
