5547fdd507b9a7fa5de6bbf232f9e7a6c1ffa3c5
M. Eren Akbiyik <erenakbiyik@gmail.com>
2022-07-31 12:32:52 +0200

    Remove remnants of Test Adapter dep.

--- test/runTest.ts
+++ test/runTest.ts
@@ -11,17 +11,21 @@ async function main() {
         const extensionDevelopmentPath = path.resolve(__dirname, "../../");
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
         const vscodeExecutablePath = await downloadAndUnzipVSCode("1.40.1");
-        const testRepoPath = path.join(__dirname, "..", "..", "test", "testRepo")
+        const testRepoPath = path.join(__dirname, "..", "..", "test", "testRepo");
         const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
 
         // Use cp.spawn / cp.exec for custom setup
-        cp.spawnSync(cliPath, ["--install-extension", "hbenl.vscode-test-explorer"], {
+        cp.spawnSync(cliPath, {
             encoding: "utf-8",
             stdio: "inherit",
         });
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs:[testRepoPath] });
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: [testRepoPath],
+        });
     } catch (err) {
         console.error(err);
         console.error("Failed to run tests");
