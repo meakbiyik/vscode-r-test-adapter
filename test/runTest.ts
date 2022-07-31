acee397e22a24c2953733b7abbc43733eae435bd
M. Eren Akbiyik <erenakbiyik@gmail.com>
2022-07-31 12:42:33 +0200

    Use new vscode-test package @vscode/test-electron

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,24 +1,11 @@
 import * as path from "path";
-import * as cp from "child_process";
-import {
-    downloadAndUnzipVSCode,
-    resolveCliPathFromVSCodeExecutablePath,
-    runTests,
-} from "vscode-test";
+import { runTests } from "@vscode/test-electron";
 
 async function main() {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, "../../");
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-        const vscodeExecutablePath = await downloadAndUnzipVSCode("1.69.2");
         const testRepoPath = path.join(__dirname, "..", "..", "test", "testRepo");
-        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
-
-        // Use cp.spawn / cp.exec for custom setup
-        cp.spawnSync(cliPath, {
-            encoding: "utf-8",
-            stdio: "inherit",
-        });
 
         // Download VS Code, unzip it and run the integration test
         await runTests({
